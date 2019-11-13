d2d28fbfd1972bac94053cee7ed3f0dc4d7c0c9f
Nadim Mahmud <mahmudnadim16@gmail.com>
2019-11-13 16:04:34 +0600

    Add files via upload
    Random Pixel added

---
+++ Random_pixel.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+
+int main() {
+    int i,j,x,y=rand()%6,cn=0;
+
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"");
+
+    while(1){
+        cn++;
+        putpixel (rand()%400,rand()%400,rand()%16);;
+        delay(rand()%400);
+        if(cn>=y){
+            cleardevice();
+            y=rand()%6;
+        }
+    }
+    getch();
+    closegraph();
+}
+
+
+
